07de1146584b78ecf76988017d866542aec07dd2
Student_04 <Student_04@DESKTOP-M6L6ME5>
2025-10-03 10:28:07 +0300

    Добавьте файлы проекта.

---
+++ FirsovaAlekseevaLab6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FirsovaAlekseevaLab6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
